f7c134278df1ebf1ee8ecfdf1ff1ced2e68b055e
Mat2OO1 <mateuszt.krajewski@gmail.com>
2023-03-13 14:01:46 +0100

    Uploaded project 1 and 2

---
+++ Bootstrap/MDB-Free_4.20.0/js/scripts.js
@@ -0,0 +1,20 @@
+AOS.init({
+    duration: 1200,
+  })  
+
+//pie
+var ctxP = document.getElementById("pieChart").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+  type: 'pie',
+  data: {
+    labels: ["Japan", "USA", "Italy", "Australia", "Thailand"],
+    datasets: [{
+      data: [150000, 400000, 1000000, 100000, 340000],
+      backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+      hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+    }]
+  },
+  options: {
+    responsive: true
+  }
+});
